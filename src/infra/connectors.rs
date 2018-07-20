d1255c08c0d728c54ff4782a8ed1d5e20e17a2c1
Fabio Gibson <fabiogibson@hotmail.com>
2018-07-20 18:45:00 -0300

    bug fix

--- src/infra/connectors.rs
+++ src/infra/connectors.rs
@@ -4,17 +4,26 @@ use r2d2_redis;
 pub struct ConnectionPoolBuilder {}
 
 impl ConnectionPoolBuilder {
-    fn build<T: r2d2::ManageConnection>(manager: T, max_size: Option<u32>) -> r2d2::Pool<T> {
-        r2d2::Pool::builder()
+    fn build<T: r2d2::ManageConnection>(
+        manager: T,
+        max_size: Option<u32>,
+    ) -> Result<r2d2::Pool<T>, ()> {
+        match r2d2::Pool::builder()
             .max_size(max_size.unwrap_or(10))
             .build(manager)
-            .unwrap()
+        {
+            Ok(pool) => Ok(pool),
+            Err(e) => Err(),
+        }
     }
 
     pub fn build_redis(host: String, db: u32) -> r2d2::Pool<r2d2_redis::RedisConnectionManager> {
         let connection_string = format!{"redis://{}/{}", host, db};
         let manager = r2d2_redis::RedisConnectionManager::new(connection_string.as_str()).unwrap();
-        ConnectionPoolBuilder::build(manager, None)
+        match ConnectionPoolBuilder::build(manager, None) {
+            Ok(pool) => pool,
+            Err() => panic!("Could not connect to redis instance on {}/{}", host, db),
+        }
     }
 
     pub fn build_postgres(
@@ -33,6 +42,6 @@ impl ConnectionPoolBuilder {
             r2d2_postgres::TlsMode::None,
         ).unwrap();
 
-        r2d2::Pool::builder().max_size(32).build(manager).unwrap()
+        ConnectionPoolBuilder::build(manager, Some(32))
     }
 }
